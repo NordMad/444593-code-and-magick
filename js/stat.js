f1810993fb13bd5d4ec8684cc0a689565cf3d3e0
Andrey Pankov <30828361+NordMad@users.noreply.github.com>
2017-11-21 18:52:21 +0500

    Объявил функции

--- js/stat.js
+++ js/stat.js
@@ -11,7 +11,7 @@ var getMaxTime = function (times) {
   return max;
 };
 
-window.renderStatistics = function (ctx, names, times) {
+var drawCloud = function (ctx) {
   ctx.fillStyle = '#000';
   ctx.strokeRect(110, 20, 420, 270);
   ctx.fillRect(110, 20, 420, 270);
@@ -25,27 +25,33 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillText('Ура вы победили!', 240, 30);
   ctx.fillText('Список результатов:', 225, 50);
+};
 
+var drawColumn = function (ctx, time, name, positionX, positionY, barWidth, histogramHeight, step) {
+  var lineHeight = 20;
+  if (name === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  } else {
+    var r = 0.3 + 0.7 * Math.random();
+    ctx.fillStyle = 'rgba(0, 0, 255, ' + r + ')';
+  }
+  ctx.fillRect(positionX, positionY, barWidth, -time * step);
+  ctx.fillStyle = '#000';
+  ctx.fillText(name, positionX, positionY + lineHeight);
+  ctx.fillText(Math.floor(time), positionX, positionY - (lineHeight + histogramHeight));
+};
+
+window.renderStatistics = function (ctx, names, times) {
   var histogramHeight = 150;
   var step = histogramHeight / getMaxTime(times);
   var barWidth = 40;
   var indent = 90;
   var initialX = 150;
   var initialY = 250;
-  var lineHeight = 20;
+
+  drawCloud(ctx);
 
   for (var j = 0; j < times.length; j++) {
-    var time = times[j];
-    var name = names[j];
-    if (name === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      var r = 0.3 + 0.7 * Math.random();
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + r + ')';
-    }
-    ctx.fillRect(initialX + indent * j, initialY, barWidth, -time * step);
-    ctx.fillStyle = '#000';
-    ctx.fillText(name, initialX + indent * j, initialY + lineHeight);
-    ctx.fillText(Math.floor(time), initialX + indent * j, initialY - (lineHeight + histogramHeight));
+    drawColumn(ctx, times[j], names[j], initialX + indent * j, initialY, barWidth, histogramHeight, step);
   }
 };
